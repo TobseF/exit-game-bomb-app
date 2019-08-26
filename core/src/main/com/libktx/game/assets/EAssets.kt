be31fce03f29315e831e9294b3318b667c737bf0
Tobse <Tobse4Git@gmail.com>
2019-08-26 19:14:05 +0200

    🔨 Restructure `EAssets`

--- core/src/main/com/libktx/game/assets/EAssets.kt
+++ core/src/main/com/libktx/game/assets/EAssets.kt
@@ -3,36 +3,52 @@ package com.libktx.game.assets
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.graphics.g2d.BitmapFont
-import ktx.assets.getAsset
+import ktx.assets.Asset
 import ktx.assets.load
 
 // sounds
-enum class SoundAssets(val path: String) {
-    MessageRight("sounds/correct_message.mp3"),
-    MessageWrong("sounds/wrong_message.mp3"),
-    BombActivated("sounds/bomb_activated_voice.mp3"),
-    BombDeactivated("sounds/bomb_deactivated.mp3"),
-    BombExplosion("sounds/bomb_explosion.mp3")
+enum class SoundAssets(override val file: String) : AssetReference {
+    MessageRight("correct_message.mp3"),
+    MessageWrong("wrong_message.mp3"),
+    BombActivated("bomb_activated_voice.mp3"),
+    BombDeactivated("bomb_deactivated.mp3"),
+    BombExplosion("bomb_explosion.mp3");
+
+    override val folder = "sounds"
 }
 
-fun AssetManager.load(asset: SoundAssets) = load<Sound>(asset.path)
-operator fun AssetManager.get(asset: SoundAssets) = getAsset<Sound>(asset.path)
+fun AssetManager.load(asset: SoundAssets) = load<Sound>(asset)
+operator fun AssetManager.get(asset: SoundAssets) = getAsset<Sound>(asset)
 
 
 // fonts
-enum class FontAssets(val path: String) {
-    Counter("fonts/counter.fnt"),
-    CounterBig("fonts/counter_big.fnt"),
-    ConsolasBig("fonts/consolas_big.fnt"),
-    Consolas("fonts/consolas_20.fnt"),
-    NumbersBig("fonts/numbers_big.fnt")
+enum class FontAssets(override val file: String) : AssetReference {
+    Counter("counter.fnt"),
+    CounterBig("counter_big.fnt"),
+    ConsolasBig("consolas_big.fnt"),
+    Consolas("consolas_20.fnt"),
+    NumbersBig("numbers_big.fnt");
+
+    override val folder = "fonts"
 }
 
-fun AssetManager.load(asset: FontAssets) = load<BitmapFont>(asset.path)
-operator fun AssetManager.get(asset: FontAssets) = getAsset<BitmapFont>(asset.path)
+fun AssetManager.load(asset: FontAssets) = load<BitmapFont>(asset)
+operator fun AssetManager.get(asset: FontAssets) = getAsset<BitmapFont>(asset)
 
 // app icons
-enum class Icons(val path: String) {
-    AppIcon64("icons/icon_64.png"),
-    AppIcon16("icons/icon_16.png"),
+enum class Icons(override val file: String) : AssetReference {
+    AppIcon64("icon_64.png"),
+    AppIcon16("icon_16.png");
+
+    override val folder = "icons"
 }
+
+interface AssetReference {
+    val file: String
+    val folder: String
+    val path: String
+        get() = "$folder/$file"
+}
+
+inline fun <reified Type : Any> AssetManager.getAsset(reference: AssetReference): Type = this[reference.path]
+inline fun <reified Type : Any> AssetManager.load(reference: AssetReference): Asset<Type> = load(reference.path)
